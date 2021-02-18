8eb579e7206ec48d5b67e92ec6d054ed4e1386a8
sangeeth <sangeethsimon2@gmail.com>
2021-02-18 15:12:06 +0530

    Set grid to 0.0 from cell [2]
    Multi-line description of commit,
    feel free to be detailed.
    
    [Ticket: X]

--- HLL/grid.c
+++ HLL/grid.c
@@ -8,9 +8,9 @@ void grid()
 	dx = L/N;
 	cell_xc=(double *)malloc((N+4)*sizeof(double));
 	
-	for(i=0;i<=N+3;i++)
+	for(i=1;i<=N+3;i++)
 	{
-		cell_xc[i] = i*dx-dx/2; //x coordinate of cell centers
+		cell_xc[i] = (i-1)*dx-dx/2; //x coordinate of cell centers
 		
 
 	}
