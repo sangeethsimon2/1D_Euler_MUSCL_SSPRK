b8c3e40c34ef9ff469d590e7c3703d2b98672f43
sangeeth <sangeethsimon2@gmail.com>
2021-02-18 14:34:44 +0530

    Extend the grid by 4 boundary elements
    In the present grid, 0, 1 and Nx+2, Nx+3 are the boundary elements
    while cells from 2 to Nx+1 are the internal cells.
    
    Multi-line description of commit,
    feel free to be detailed.
    
    [Ticket: X]

--- HLL/grid.c
+++ HLL/grid.c
@@ -6,9 +6,9 @@ void grid()
 {
 	int i;
 	dx = L/N;
-	cell_xc=(double *)malloc((N+2)*sizeof(double));
+	cell_xc=(double *)malloc((N+4)*sizeof(double));
 	
-	for(i=0;i<=N+1;i++)
+	for(i=0;i<=N+3;i++)
 	{
 		cell_xc[i] = i*dx-dx/2; //x coordinate of cell centers
 		
